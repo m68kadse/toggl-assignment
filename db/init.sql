490cde0405ab2d81a0ac42204fa18a82b1cc8888
Nicole Molly Benkö <nicolembenkoe@gmail.com>
2023-11-03 14:51:48 +0100

    add minimum db schema

---
+++ db/init.sql
@@ -0,0 +1,15 @@
+-- this can be run on start as it will not change the db schema if it already exists
+
+
+CREATE TABLE IF NOT EXISTS question(
+    id PRIMARY KEY,
+    body TEXT NOT NULL
+);
+
+CREATE TABLE IF NOT EXISTS "option"(
+    id PRIMARY KEY,
+    fk_question INTEGER NOT NULL,
+    body TEXT NOT NULL,
+    correct INTEGER NOT NULL,
+    FOREIGN KEY(fk_question) REFERENCES question(id)
+);
